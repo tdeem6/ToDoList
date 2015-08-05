a8f73d59fad9fd410b06c1f854680156d75f7252
Tyler Deem <tdeem6@gmail.com>
2015-08-05 08:29:05 -0700

    Added new task also added new  superglobal for description.

--- app/app.php
+++ app/app.php
@@ -17,16 +17,26 @@
         $output = $output . "<p>" . $task->getDescription() . "</p>";
       }
 
-      $output = $output . "</ul>
-      <form action='/tasks' method='post'>
-          <label for='description'>Task Description</label>
-          <input id='description' name='description' type='text'>
+        $output = $output . "</ul>
+        <form action='/tasks' method='post'>
+            <label for='description'>Task Description</label>
+            <input id='description' name='description' type='text'>
 
-          <button type='submit'>Add task</button>
-      </form>
-  ";
+            <button type='submit'>Add task</button>
+        </form>
+      ";
 
+      $app->post("/tasks", function() {
+              $task = new Task($_POST['description']);
+              $task->save();
+              return "
+                  <h1>You created a task!</h1>
+                  <p>" . $task->getDescription() . "</p>
+                  <p><a href='/'>View your list of things to do.</a></p>
+              ";
         return $output;
+
+
     });
 
     return $app;
